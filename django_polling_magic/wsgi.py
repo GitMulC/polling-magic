c25ab4fc9bbc92c731c97d899aca4f9e9f467ea0
GitMulC <cmulvo@gmail.com>
2022-05-09 19:28:05 +0000

    Edit names from polling_magic to django_polling_magic

--- django_polling_magic/wsgi.py
+++ django_polling_magic/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polling_magic.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_polling_magic.settings')
 
 application = get_wsgi_application()
