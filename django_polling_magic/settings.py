4e13d8bef9ac84a958c72676a137cfbc0474949f
GitMulC <cmulvo@gmail.com>
2022-05-12 19:08:14 +0000

    Fix code errors

--- django_polling_magic/settings.py
+++ django_polling_magic/settings.py
@@ -121,7 +121,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cludinary_storage.storage.MediaCludinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
