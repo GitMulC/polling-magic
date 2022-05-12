1131efc94cccacfb8db219dfc194f5fe93909bd8
GitMulC <cmulvo@gmail.com>
2022-05-12 19:17:36 +0000

    Add data models for poll & comments

--- polling_magic/models.py
+++ polling_magic/models.py
@@ -25,7 +25,7 @@ class Poll(models.Model):
         return self.dislikes.count()
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Poll, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
