e4c08fa8aac202b15f7851b883852bc8e6125649
GitMulC <cmulvo@gmail.com>
2022-05-16 15:03:34 +0000

    Add search fields & filters for admin

--- polling_magic/admin.py
+++ polling_magic/admin.py
@@ -6,4 +6,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Poll)
 class PostAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('set',)}
+    list_filter = ('release_date',)
+    list_display = ('set', 'release_date',)
+    search_fields = ['set', 'lore']
     summernote_fields = ('lore')
