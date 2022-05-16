36982154102b998fe2abad1a2074014a84443bba
GitMulC <cmulvo@gmail.com>
2022-05-16 14:09:59 +0000

    Edit models

--- polling_magic/models.py
+++ polling_magic/models.py
@@ -5,6 +5,7 @@ from cloudinary.models import CloudinaryField
 
 class Poll(models.Model):
     set = models.CharField(max_length=250, null=False, blank=False)
+    slug = models.SlugField(max_length=200, null=False, blank=False)
     image = CloudinaryField('image', null=False, blank=False)
     new_cards = models.IntegerField(default='0', null=False, blank=False)
     set_icon = CloudinaryField('image', null=False, blank=False)
@@ -13,7 +14,6 @@ class Poll(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='set_likes', blank=True)
     dislikes = models.ManyToManyField(User, related_name='set_dislikes', blank=True)
-    slug = models.SlugField(max_length=200, null=False, blank=False)
 
     def __str__(self):
         return self.set
