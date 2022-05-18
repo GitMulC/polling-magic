5df9f0a5a1c2b655278e741f00bb4c3181b47235
GitMulC <cmulvo@gmail.com>
2022-05-18 20:40:25 +0000

    Add content to template index.html

--- polling_magic/views.py
+++ polling_magic/views.py
@@ -5,6 +5,6 @@ from .models import Poll
 
 class PollList(generic.ListView):
     model = Poll
-    # queryset = Poll.objects.filter(status=1).order_by('-release_date')
+    # queryset = Poll.objects.filter(status=1).order_by('-release_date') Status needed?
     template_name = 'index.html'
     paginate_by = 6
