595d2de2e7527bb48b9201316c95d261d716e055
GitMulC <cmulvo@gmail.com>
2022-05-18 18:57:15 +0000

    Add poll class to views

--- polling_magic/views.py
+++ polling_magic/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.views import generic
+from .models import Poll
 
-# Create your views here.
 
-
-def say_hello(request):
-    return HttpResponse("Hello!")
+class PollList(generic.ListView):
+    model = Poll
+    # queryset = Poll.objects.filter(status=1).order_by('-release_date')
+    template_name = 'index.html'
+    paginate_by = 6
