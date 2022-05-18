7a2244c0080c549e59a05b8e89ca1ef8108062a7
GitMulC <cmulvo@gmail.com>
2022-05-18 18:56:31 +0000

    Remove test coding from urls

--- django_polling_magic/urls.py
+++ django_polling_magic/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from polling_magic.views import say_hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', say_hello, name='hello'),
     path('summernote/', include('django_summernote.urls')),
 ]
