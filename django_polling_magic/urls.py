d1d4627309debd60571bab2c671d09154d4c3fc6
GitMulC <cmulvo@gmail.com>
2022-05-19 23:59:44 +0000

    Create new urls file & link urls together

--- django_polling_magic/urls.py
+++ django_polling_magic/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('', include('polling_magic.urls'), name='polling_magic_urls'),
 ]
