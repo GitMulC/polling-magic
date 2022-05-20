0fb290fc5dda0f5b6a900a32ac926dc6ce095d12
GitMulC <cmulvo@gmail.com>
2022-05-20 17:09:33 +0000

    Add view_poll to urls

--- polling_magic/urls.py
+++ polling_magic/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PollList.as_view(), name='home')
+    path('', views.PollList.as_view(), name='home'),
+    path('poll/<int:id>/', views.view_poll, name='view_poll'),
 ]
