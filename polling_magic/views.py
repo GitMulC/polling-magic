41830bcda2da2c6ec16d0c2d037351a609894325
GitMulC <cmulvo@gmail.com>
2022-05-20 17:10:10 +0000

    Add custome view_poll to views

--- polling_magic/views.py
+++ polling_magic/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Poll
+from .models import Poll, Comment
 
 
 class PollList(generic.ListView):
     model = Poll
-    # queryset = Poll.objects.filter(status=1).order_by('-release_date') Status needed?
+    # queryset = Poll.objects.filter(status=1).order_by('created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+def view_poll(request, id):
+    poll = get_object_or_404(Poll, id=id)
+    comments = Comment.objects.filter(post=poll, approved=True)
+    template = 'poll.html'
+    context = {
+        'poll': poll,
+        'comments': comments,
+    }
+    return render(request, template, context)
