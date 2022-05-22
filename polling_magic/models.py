e9840a3dbea921e7aecb55583857f098dfd0c5f5
GitMulC <cmulvo@gmail.com>
2022-05-22 14:22:42 +0000

    Edit poll model names for clarity

--- polling_magic/models.py
+++ polling_magic/models.py
@@ -6,9 +6,9 @@ from cloudinary.models import CloudinaryField
 class Poll(models.Model):
     set = models.CharField(max_length=250, null=False, blank=False)
     slug = models.SlugField(max_length=200, null=False, blank=False)
-    image = CloudinaryField('image', null=False, blank=False)
+    image = CloudinaryField('set image', null=False, blank=False)
     new_cards = models.IntegerField(default='0', null=False, blank=False)
-    set_icon = CloudinaryField('image', null=False, blank=False)
+    set_icon = CloudinaryField('set icon', null=False, blank=False)
     lore = models.TextField(null=False, blank=False)
     release_date = models.DateField(null=False, blank=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
