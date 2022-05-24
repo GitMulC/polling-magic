c0233705c11342b7a33c96fc452432be1695ce44
GitMulC <cmulvo@gmail.com>
2022-05-24 17:42:18 +0000

    Add allauth to apps, & link to path in urls file

--- polling_magic/urls.py
+++ polling_magic/urls.py
@@ -1,7 +1,8 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.PollList.as_view(), name='home'),
     path('poll/<int:id>/', views.view_poll, name='view_poll'),
+    path('accounts/', include('allauth.urls')),
 ]
