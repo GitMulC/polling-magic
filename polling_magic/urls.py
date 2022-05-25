36ab6215aabf701e20ac2e2207b563d54281b272
GitMulC <cmulvo@gmail.com>
2022-05-25 23:40:10 +0000

    Comment off alternate commenting system url paths & import

--- polling_magic/urls.py
+++ polling_magic/urls.py
@@ -1,11 +1,11 @@
 from . import views
 from django.urls import path, include
-from .views import AddCommentView
+# from .views import AddCommentView
 
 urlpatterns = [
     path('', views.PollList.as_view(), name='home'),
     path('poll/<int:id>/', views.view_poll, name='view_poll'),
     path('accounts/', include('allauth.urls')),
     path('home', views.home),
-    path('poll/<int:id>/comment/', AddCommentView.as_view(), name='add_comment'), # noqa
+    # path('poll/<int:pk>/comment/', AddCommentView.as_view(), name='add_comment'),
 ]
