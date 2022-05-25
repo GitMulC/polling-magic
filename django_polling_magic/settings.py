01f354631aeb1eea89534393ba26150556cfac8b
GitMulC <cmulvo@gmail.com>
2022-05-25 22:23:27 +0000

    Add forms.py file, link crispy forms in settings.py

--- django_polling_magic/settings.py
+++ django_polling_magic/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'polling_magic',
 ]
 
@@ -44,6 +45,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 MIDDLEWARE = [
