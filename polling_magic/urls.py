b032d1fbac19d5e8e54a800871d9e22f1794c447
GitMulC <cmulvo@gmail.com>
2022-05-26 20:38:39 +0000

    Comment out bugged code

--- polling_magic/urls.py
+++ polling_magic/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', views.PollList.as_view(), name='home'),
     path('poll/<int:id>/', views.view_poll, name='view_poll'),
+    # path('poll/<int:id>/', views.PollView.as_view(), name='view_poll'),
     path('accounts/', include('allauth.urls')),
     path('home', views.home),
     # path('poll/<int:pk>/comment/', AddCommentView.as_view(), name='add_comment'),
