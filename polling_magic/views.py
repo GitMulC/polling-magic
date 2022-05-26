e83c057733aa960ee266f743c9ccffb7877c998a
GitMulC <cmulvo@gmail.com>
2022-05-26 20:39:17 +0000

    Attempt at adding working commenting system to site on views.py

--- polling_magic/views.py
+++ polling_magic/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic
-from django.views.generic.edit import CreateView
+from django.views import generic, View
+# from django.views.generic.edit import CreateView
 from .models import Poll, Comment
 from .forms import CommentForm
 
@@ -11,41 +11,51 @@ class PollList(generic.ListView):
     paginate_by = 6
 
 
+
 def view_poll(request, id):
-    poll = get_object_or_404(Poll, id=id)
-    comments = Comment.objects.filter(post=poll, approved=True)
-    template = 'poll.html'
-    context = {
-        'poll': poll,
-        'comments': comments,
-        'comment_form': CommentForm()
+        poll = get_object_or_404(Poll, id=id)
+        comments = Comment.objects.filter(post=poll, approved=True)
+        template = 'poll.html'
+        context = {
+            'poll': poll,
+            'comments': comments,
+            'commented': False,
+            'comment_form': CommentForm()
+        }
+        return render(request, template, context,)
 
-    }
+# class PollView(View):
 
-    new_comment = None
-    # Comment posted
-    if request.method == 'POST':
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
-            comment_form.instance.email = request.user.email
-            comment_form.instance.name = request.user.username
-            # Create Comment object but don't save to database yet
-            new_comment = comment_form.save(commit=False)
-            # Assign the current post to the comment
-            new_comment.poll = poll
-            # Save the comment to the database
-            new_comment.save()
-    else:
-        comment_form = CommentForm()
-    return render(request, template, context)
+    # def view_poll(request, id):
+    #     poll = get_object_or_404(Poll, id=id)
+    #     comments = Comment.objects.filter(post=poll, approved=True)
+    #     template = 'poll.html'
+    #     context = {
+    #         'poll': poll,
+    #         'comments': comments,
+    #         'commented': True,
+    #         'comment_form': CommentForm()
+    #     }
+
+    #     comment_form = CommentForm(data=request.POST)
+    #     if comment_form.is_valid():
+    #         comment_form.instance.email = request.user.email
+    #         comment_form.instance.name = request.user.username
+    #         comment = comment_form.save(commit=False)
+    #         comment.poll = poll
+    #         comment.save()
+    #     else:
+    #         comment_form = CommentForm()
+
+    #     return render(request, template, context,)
+
 
 
 # class AddCommentView(CreateView):
-#     model = Comment
-#     # form_class = Form
-#     template = 'add_comment.html'
+#    model = Comment
+#    # form_class = Form
+#    template = 'add_comment.html'
 #     fields = '__all__'
-
 
 def home(request):
     return render(request, 'templates/index')
