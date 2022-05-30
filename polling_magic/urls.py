514d9820b1abc233eba16dfec298c498120a6233
GitMulC <cmulvo@gmail.com>
2022-05-30 18:39:17 +0000

    Add CRUD functionality to poll.html, format urls, create edit, delete & comment views

--- polling_magic/urls.py
+++ polling_magic/urls.py
@@ -1,13 +1,17 @@
-from . import views
 from django.urls import path, include
-# from .views import AddCommentView
+from . import views
 
 urlpatterns = [
     path('', views.PollList.as_view(), name='home'),
-    path('poll/<int:id>/', views.view_poll, name='view_poll'),
-    path('like/<int:id>', views.PollLike.as_view(), name='poll_like'),
-    # path('poll/<int:id>/', views.PollView.as_view(), name='view_poll'),
-    path('accounts/', include('allauth.urls')),
     path('home', views.home),
-    # path('poll/<int:pk>/comment/', AddCommentView.as_view(), name='add_comment'),
+    path('poll/<int:id>/', views.view_poll, name='view_poll'),
+    path('like/<int:id>', views.poll_like, name='poll_like'),
+    path('dislike/<int:id>', views.poll_dislike, name='poll_dislike'),
+    path('accounts/', include('allauth.urls')),
+    path(
+        'poll/edit/<int:p_id>/<int:c_id>/',
+        views.edit_comment, name='edit_comment'),
+    path(
+        'poll/delete/<int:p_id>/<int:c_id>/',
+        views.delete_comment, name='delete_comment'),
 ]
--- polling_magic/views.py
+++ polling_magic/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views import generic, View
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 # from django.views.generic.edit import CreateView
 from .models import Poll, Comment
 from .forms import CommentForm
+
+
+def home(request):
+    return render(request, 'templates/index')
 
 
 class PollList(generic.ListView):
@@ -12,18 +17,52 @@ class PollList(generic.ListView):
     paginate_by = 6
 
 
-
 def view_poll(request, id):
-        poll = get_object_or_404(Poll, id=id)
-        comments = Comment.objects.filter(post=poll, approved=True)
-        template = 'poll.html'
-        context = {
-            'poll': poll,
-            'comments': comments,
-            'commented': False,
-            'comment_form': CommentForm()
-        }
-        return render(request, template, context,)
+    poll = get_object_or_404(Poll, id=id)
+    comments = Comment.objects.filter(post=poll, approved=True)
+    if request.method == "POST":
+        comment_form = CommentForm(request.POST or None)
+        if comment_form.is_valid():
+            comment_form.instance.name = request.user.username
+            comment_form.instance.body = request.POST.get("body")
+            comment_form.instance.approved = True
+            comment_form.instance.post = poll
+            comment_form.instance.save()
+            messages.success(request, "Comment Added")
+        else:
+            messages.error(request, "Error: Please Try Again.")
+    comment_form = CommentForm()
+    template = 'poll.html'
+    context = {
+        'poll': poll,
+        'comments': comments,
+        'commented': False,
+        'comment_form': comment_form
+    }
+    return render(request, template, context)
+
+
+def edit_comment(request, p_id, c_id):
+    poll = get_object_or_404(Poll, id=p_id)
+    comment = get_object_or_404(Comment, id=c_id)
+    comment_form = CommentForm(request.POST or None, instance=comment)
+    if comment_form.is_valid():
+        comment_form.instance.name = request.user.username
+        comment_form.instance.body = request.POST.get("body")
+        comment_form.instance.approved = True
+        comment_form.instance.post = poll
+        comment_form.instance.save()
+        messages.success(request, "Comment Updated")
+    else:
+        messages.error(request, "Error: Please Try Again.")
+    return redirect("view_poll", p_id)
+
+
+def delete_comment(request, p_id, c_id):
+    poll = get_object_or_404(Poll, id=p_id)
+    comment = get_object_or_404(Comment, id=c_id)
+    comment.delete()
+    return redirect("view_poll", p_id)
 
 # class PollView(View):
 
@@ -58,16 +97,32 @@ def view_poll(request, id):
 #    template = 'add_comment.html'
 #     fields = '__all__'
 
-def home(request):
-    return render(request, 'templates/index')
 
-class PollLike(View):
-    def post(self, request, slug):
-        poll = get_object_or_404(Poll, slug=slug)
+# class PollLike(View):
+#     def post(self, request, slug):
+#         poll = get_object_or_404(Poll, slug=slug)
 
-        if poll.likes.filter(id-request.user.id).exists():
-            poll.likes.remove(request.user)
-        else:
-            poll.likes.add(request.user)
+#         if poll.likes.filter(id-request.user.id).exists():
+#             poll.likes.remove(request.user)
+#         else:
+#             poll.likes.add(request.user)
 
-        return HttpResponseRedirect(reverse('view_poll'))
+#         return HttpResponseRedirect(reverse('view_poll'))
+
+
+def poll_like(request, id):
+    poll = get_object_or_404(Poll, id=id)
+    if poll.likes.filter(id=request.user.id).exists():
+        poll.likes.remove(request.user)
+    else:
+        poll.likes.add(request.user)
+    return redirect("view_poll", id)
+
+
+def poll_dislike(request, id):
+    poll = get_object_or_404(Poll, id=id)
+    if poll.dislikes.filter(id=request.user.id).exists():
+        poll.dislikes.remove(request.user)
+    else:
+        poll.dislikes.add(request.user)
+    return redirect("view_poll", id)
